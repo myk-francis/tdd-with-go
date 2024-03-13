050030a3e5fe3bb198d1d0ac998e7516f9aedd42
myk <michaelmugendi1707@gmail.com>
2024-03-13 13:47:34 +0300

    Update: refactored tests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -10,3 +10,26 @@ func TestHello(t *testing.T)  {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestHelloTwo(t *testing.T)  {
+	assertCorrectMessage := func (t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+	t.Run("saying hello to people", func (t *testing.T)  {
+		got := Hello("Chris")
+		want := "Hello, Chris!"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("say 'Hello, World!' when an empty string is provided", func (t *testing.T)  {
+		got := Hello("")
+		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
