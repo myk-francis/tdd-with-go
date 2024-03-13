bec9726fd3fad3b48e3676d456ab675ee4feed2a
myk <michaelmugendi1707@gmail.com>
2024-03-13 14:19:22 +0300

    Update: all tests passing

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHello(t *testing.T)  {
-	got := Hello("Chris")
+	got := Hello("Chris", "English")
 	want := "Hello, Chris!"
 
 	if got != want {
@@ -19,7 +19,7 @@ func TestHelloTwo(t *testing.T)  {
 		}
 	}
 	t.Run("saying hello to people", func (t *testing.T)  {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris!"
 
 		assertCorrectMessage(t, got, want)
@@ -27,8 +27,15 @@ func TestHelloTwo(t *testing.T)  {
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is provided", func (t *testing.T)  {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func (t *testing.T)  {
+		got := Hello("Myk", "Spanish")
+		want := "Hola, Myk!"
 
 		assertCorrectMessage(t, got, want)
 	})
