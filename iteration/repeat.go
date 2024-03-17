a29e3bad0793aa0208e6837d842a9ac3e0a5c2e1
myk <michaelmugendi1707@gmail.com>
2024-03-17 11:53:47 +0300

    Update: took input and updated function repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,25 @@
-package iteration
+package main
 
-func Repeat(character string) string {
+import "fmt"
+
+func Repeat(character string, numberOfIterations int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	
+	for i := 0; i < numberOfIterations; i++ {
+		repeated += character
 	}
 	return repeated
+}
+
+func main() {
+	var numOfIteration int
+
+	fmt.Print("How many times should a be repeate?:")
+	_,err := fmt.Scan(&numOfIteration)
+	if err != nil {
+		fmt.Println("Something went wrong!!")
+		return 
+	}
+
+	fmt.Println(Repeat("a", 5))
 }
