cedfee8adf88bb9281c3b0df01973be7051fe1e3
myk <michaelmugendi1707@gmail.com>
2024-03-19 13:12:15 +0300

    Update: first test passed

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,13 +1,14 @@
 package main
 
 type Wallet struct {
+	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	//any variable or function name start with a small letter is private opposite is public
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
