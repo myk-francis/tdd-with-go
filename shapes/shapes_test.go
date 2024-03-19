2e3bc5cb78d3ab2cfd8a90564119c2af0c9df763
myk <michaelmugendi1707@gmail.com>
2024-03-19 11:29:24 +0300

    Update: implemented triangle shape

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -32,3 +32,20 @@ func TestArea(t *testing.T) {
 	})
 }
 
+func TestAreaTable(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
+
