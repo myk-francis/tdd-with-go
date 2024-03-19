1f644580030e7ea493e0a5ef2718e4d677794281
myk <michaelmugendi1707@gmail.com>
2024-03-19 14:06:35 +0300

    Update: tests pass

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -31,6 +31,13 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
 	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Helper()
 		got := wallet.Balance()
@@ -52,5 +59,15 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
 	})
 }
