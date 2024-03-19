a51f33af6459ffe9771829c254368fd650470e20
myk <michaelmugendi1707@gmail.com>
2024-03-19 11:41:16 +0300

    Update: finished table testing

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -34,18 +34,22 @@ func TestArea(t *testing.T) {
 
 func TestAreaTable(t *testing.T) {
 	areaTests := []struct {
+		name string
 		shape Shape
-		want float64
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle",shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle",shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle",shape: Triangle{12, 6}, hasArea: 36.0},
 	}
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// using tt.name from the case to use it as the `t.Run`
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
 
