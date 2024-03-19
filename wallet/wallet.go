1c341c57b47a7f81f314794bd4f659c697b0c717
myk <michaelmugendi1707@gmail.com>
2024-03-19 13:06:49 +0300

    Update: added implementation of Deposit and Balance

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,1 +1,13 @@
 package main
+
+type Wallet struct {
+}
+
+func (w Wallet) Deposit(amount int) {
+	//any variable or function name start with a small letter is private opposite is public
+	w.balance += amount
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestWallet(t *testing.T) {
 
 	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	wallet.Deposit(10)
 
 	got := wallet.Balance()
 	want := 10
