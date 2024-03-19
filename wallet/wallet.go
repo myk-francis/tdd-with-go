f5e6b16030871487ddb32475fb74b37121dc9ffb
myk <michaelmugendi1707@gmail.com>
2024-03-19 13:15:56 +0300

    Update: created new int type Bitcoin

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,14 +1,16 @@
 package main
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	//any variable or function name start with a small letter is private opposite is public
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
