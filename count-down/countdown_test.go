f7aec70f6141402ffb71a9491c0d97ba097fd481
myk <michaelmugendi1707@gmail.com>
2024-03-21 10:54:54 +0300

    Update: added multiple tests

--- count-down/countdown_test.go
+++ count-down/countdown_test.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountDown(t *testing.T) {
-	buffer := &bytes.Buffer{}
+	t.Run("sleep before every print", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
 	spySleeper := &SpySleeper{}
 
 	CountDown(buffer, spySleeper)
@@ -24,4 +26,24 @@ Go!`
 	if spySleeper.Calls != 4 {
 		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
 	}
+	})
+	
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		CountDown(spySleepPrinter, spySleepPrinter)
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter)
+		}
+	})
 }
