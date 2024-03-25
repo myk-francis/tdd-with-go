3fd5dfb5739f25dbe59028c23a25fdb169e05936
myk <michaelmugendi1707@gmail.com>
2024-03-25 08:54:57 +0300

    Update: added racer function

---
+++ racer/racer_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+// func TestInit(t *testing.T) {
+
+// 	t.Run("initial test", func(t *testing.T) {
+
+// 		want := fastUrl
+// 		got := Racer(slowUrl, fastUrl)
+
+// 		if got != want {
+// 			t.Errorf("got %q want %q", got, want)
+// 		}
+
+// 	})
+// }
+
+func TestRacer(t *testing.T) {
+
+	t.Run("initial test", func(t *testing.T) {
+		slowUrl := "http://www.google.com"
+		fastUrl := "http://www.facebook.com"
+
+		want := fastUrl
+		got := Racer(slowUrl, fastUrl)
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
