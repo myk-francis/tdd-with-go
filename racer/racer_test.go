61581576459575bcf3f79d7badaa53560a20ee80
myk <michaelmugendi1707@gmail.com>
2024-03-25 09:37:03 +0300

    Update: test done for now

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -16,8 +16,21 @@ import "testing"
 // 	})
 // }
 
-func TestRacer(t *testing.T) {
+func TestRacerOne(t *testing.T) {
+	t.Run("initial test", func(t *testing.T) {
+		slowUrl := "http://www.google.com"
+		fastUrl := "http://www.facebook.com"
 
+		want := fastUrl
+		got := Racer(slowUrl, fastUrl)
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func TestRacerTwo(t *testing.T) {
 	t.Run("initial test", func(t *testing.T) {
 		slowUrl := "http://www.google.com"
 		fastUrl := "http://www.facebook.com"
